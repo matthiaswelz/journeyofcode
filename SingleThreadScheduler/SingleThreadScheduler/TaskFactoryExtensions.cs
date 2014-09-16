c7830b677c6dba156d9d9cd4909e2402247887a3
matthiaswelz <github@journeyofcode.com>
2014-09-16 17:16:01 +0200

    Added extension methods for TaskFactory.

---
+++ SingleThreadScheduler/SingleThreadScheduler/TaskFactoryExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace journeyofcode.Threading
+{
+    public static class TaskFactoryExtensions
+    {
+        public static Task StartNew(this TaskFactory factory, Action action, TaskScheduler scheduler)
+        {
+            return factory.StartNew(action, factory.CancellationToken, factory.CreationOptions, scheduler);
+        }
+        public static Task<T> StartNew<T>(this TaskFactory factory, Func<T> func, TaskScheduler scheduler)
+        {
+            return factory.StartNew(func, factory.CancellationToken, factory.CreationOptions, scheduler);
+        }
+    }
+}
