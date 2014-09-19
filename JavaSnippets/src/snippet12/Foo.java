f7196b6c38c4ea49ececd37e7d3bab20774c3497
matthiaswelz <github@journeyofcode.com>
2014-09-19 10:16:53 +0200

    Added snippet12.

---
+++ JavaSnippets/src/snippet12/Foo.java
@@ -0,0 +1,10 @@
+package snippet12;
+
+public class Foo {	
+	public static void main(String[] args) {
+		http://www.journeyofcode.com
+		System.out.print("Hello ");
+		http://www.example.org
+		System.out.print(" World!");
+	}
+}
