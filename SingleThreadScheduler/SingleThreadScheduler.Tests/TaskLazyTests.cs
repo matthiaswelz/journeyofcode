6128be143473b7995405292ee213de248828e384
matthiaswelz <github@journeyofcode.com>
2014-09-20 22:08:31 +0200

    Added TaskLazy and tests to SingleThreadScheduler.

---
+++ SingleThreadScheduler/SingleThreadScheduler.Tests/TaskLazyTests.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using journeyofcode.Threading;
+using NUnit.Framework;
+
+namespace Tests.journeyofcode.Threading
+{
+    [TestFixture]
+    public sealed class TaskLazyTests
+    {
+        [Test]
+        public void Can_Be_Constructed_From_Task()
+        {
+            var task = Task.Run(() => 42);
+            var taskLazy = new TaskLazy<int>(task);
+
+            Assert.AreEqual(task, taskLazy.Task);
+        }
+        [Test]
+        public void Can_Be_Assigned_From_Task()
+        {
+            var task = Task.Run(() => 42);
+            TaskLazy<int> taskLazy = task;
+
+            Assert.AreEqual(task, taskLazy.Task);
+        }
+
+        [Test]
+        public void Can_Be_Constructed_From_Constant()
+        {
+            var taskLazy = new TaskLazy<int>(42);
+
+            Assert.IsNotNull(taskLazy.Task);
+            Assert.IsTrue(taskLazy.IsReady);
+            Assert.AreEqual(42, taskLazy.Value);
+        }
+
+        [Test]
+        public void Can_Be_Assigned_From_Constant()
+        {
+            TaskLazy<int> taskLazy = 42;
+
+            Assert.IsNotNull(taskLazy.Task);
+            Assert.IsTrue(taskLazy.IsReady);
+            Assert.AreEqual(42, taskLazy.Value);
+        }
+
+        [Test]
+        public void Can_Be_Constructed_From_Func()
+        {
+            var taskLazy = new TaskLazy<int>(() => 42);
+
+            Assert.IsNotNull(taskLazy.Task);
+
+            taskLazy.Task.Wait();
+
+            Assert.IsTrue(taskLazy.IsReady);
+            Assert.AreEqual(42, taskLazy.Value);
+        }
+
+        [Test]
+        public void Executes_Func_On_Default_TaskScheduler()
+        {
+            var taskLazy = new TaskLazy<int>(() =>
+            {
+                Assert.AreEqual(TaskScheduler.Current, TaskScheduler.Default);
+
+                return 42;
+            });
+
+            taskLazy.Task.Wait();
+
+            Assert.AreEqual(42, taskLazy.Value);
+        }
+
+        [Test]
+        public void Can_Access_Value_After_Completion()
+        {
+            var taskCompletionSource = new TaskCompletionSource<int>();
+            var taskLazy = new TaskLazy<int>(taskCompletionSource.Task);
+
+            taskCompletionSource.SetResult(42);
+
+            Assert.AreEqual(42, taskLazy.Value);
+        }
+
+        [Test]
+        public void Cannot_Access_Value_Before_Completion()
+        {
+            var taskCompletionSource = new TaskCompletionSource<int>();
+            var taskLazy = new TaskLazy<int>(taskCompletionSource.Task);
+
+            Assert.Catch<InvalidOperationException>(() =>
+            {
+                var temp = taskLazy.Value;
+            });
+        }
+
+
+        [Test]
+        public void Implicit_Conversion_Returns_Task()
+        {
+            var task = Task.Run(() => 42);
+            TaskLazy<int> taskLazy = task;
+
+            Assert.AreEqual(task, (Task)taskLazy);
+        }
+
+        [Test]
+        public void Implicit_Conversion_Returns_Value_When_Ready()
+        {
+
+            var taskCompletionSource = new TaskCompletionSource<int>();
+            var taskLazy = new TaskLazy<int>(taskCompletionSource.Task);
+            taskCompletionSource.SetResult(42);
+
+            int actual = taskLazy;
+
+            Assert.AreEqual(42, actual);
+        }
+
+        [Test]
+        public void Implicit_Conversion_Returns_Default_When_Not_Ready()
+        {
+            var taskCompletionSource = new TaskCompletionSource<int>();
+            var taskLazy = new TaskLazy<int>(taskCompletionSource.Task);
+
+            int actual = taskLazy;
+
+            Assert.AreEqual(0, actual);
+        }
+
+        [Test]
+        public void IsReady_Returns_Correct_Value()
+        {
+            var taskCompletionSource = new TaskCompletionSource<int>();
+            var taskLazy = new TaskLazy<int>(taskCompletionSource.Task);
+
+            Assert.IsFalse(taskLazy.IsReady);
+
+            taskCompletionSource.SetResult(42);
+
+            Assert.IsTrue(taskLazy.IsReady);
+        }
+
+        [Test]
+        public void ValueOrDefault_Retuns_Value_When_Ready()
+        {
+            var taskCompletionSource = new TaskCompletionSource<int>();
+            var taskLazy = new TaskLazy<int>(taskCompletionSource.Task);
+            taskCompletionSource.SetResult(42);
+
+            var actual = taskLazy.ValueOrDefault();
+
+            Assert.AreEqual(42, actual);
+        }
+
+        [Test]
+        public void ValueOrDefault_Retuns_Default_When_Not_Ready()
+        {
+            var taskCompletionSource = new TaskCompletionSource<int>();
+            var taskLazy = new TaskLazy<int>(taskCompletionSource.Task);
+
+            var actual = taskLazy.ValueOrDefault(52);
+
+            Assert.AreEqual(52, actual);
+        }
+
+        [Test]
+        public void TryGetValue_Returns_Value_When_Ready()
+        {
+            var taskCompletionSource = new TaskCompletionSource<int>();
+            var taskLazy = new TaskLazy<int>(taskCompletionSource.Task);
+            taskCompletionSource.SetResult(42);
+
+            int actual;
+            var result = taskLazy.TryGetValue(out actual);
+
+            Assert.IsTrue(result);
+            Assert.AreEqual(42, actual);
+        }
+
+        [Test]
+        public void TryGetValue_Returns_False_When_Not_Ready()
+        {
+            var taskCompletionSource = new TaskCompletionSource<int>();
+            var taskLazy = new TaskLazy<int>(taskCompletionSource.Task);
+
+            int actual;
+            var result = taskLazy.TryGetValue(out actual);
+
+            Assert.IsFalse(result);
+            Assert.AreEqual(0, actual);
+        }
+
+        [Test]
+        public void ActionOrDefault_Uses_Action_When_Value_IsReady()
+        {
+
+            var taskCompletionSource = new TaskCompletionSource<int>();
+            var taskLazy = new TaskLazy<int>(taskCompletionSource.Task);
+            taskCompletionSource.SetResult(42);
+
+            int actual = taskLazy.ActionOrDefault(value =>
+            {
+                Assert.AreEqual(42, value);
+
+                return 52;
+            });
+
+            Assert.AreEqual(52, actual);
+        }
+
+        [Test]
+        public void ActionOrDefault_Returns_Default_When_Value_Is_Not_Ready()
+        {
+            var taskCompletionSource = new TaskCompletionSource<int>();
+            var taskLazy = new TaskLazy<int>(taskCompletionSource.Task);
+
+            int actual = taskLazy.ActionOrDefault(value =>
+            {
+                Assert.Fail();
+                return 0;
+            }, 42);
+
+            Assert.AreEqual(42, actual);
+        }
+
+        [Test]
+        public void WhenValueAvailable_Calls_Action_Immediately_If_Value_Already_Is_Available()
+        {
+            var taskCompletionSource = new TaskCompletionSource<int>();
+            var taskLazy = new TaskLazy<int>(taskCompletionSource.Task);
+            taskCompletionSource.SetResult(42);
+
+            bool called = false;
+            taskLazy.WhenValueAvailable(result =>
+            {
+                Assert.AreEqual(42, result);
+
+                called = true;
+            }).Wait();
+
+            Assert.IsTrue(called);
+        }
+
+        [Test]
+        public void WhenValueAvailableAsync_Calls_Action_In_Correct_TaskScheduler_If_Value_Already_Is_Available()
+        {
+            var taskCompletionSource = new TaskCompletionSource<int>();
+            var taskLazy = new TaskLazy<int>(taskCompletionSource.Task);
+            taskCompletionSource.SetResult(42);
+
+            bool called = false;
+            taskLazy.WhenValueAvailableAsync(result =>
+            {
+                Assert.AreEqual(42, result);
+
+                called = true;
+            }, TaskScheduler.Default).Wait();
+
+            Assert.IsTrue(called);
+        }
+
+        [Test]
+        public void When_Value_Available_Executes_When_Value_Becomes_Available()
+        {
+            var taskCompletionSource = new TaskCompletionSource<int>();
+            var taskLazy = new TaskLazy<int>(taskCompletionSource.Task);
+
+            bool called = false;
+            var task = taskLazy.WhenValueAvailable(result =>
+            {
+                Assert.AreEqual(42, result);
+
+                called = true;
+            });
+
+            Assert.IsFalse(called);
+
+            taskCompletionSource.SetResult(42);
+            task.Wait();
+
+            Assert.IsTrue(called);
+        }
+
+        [Test]
+        public void Value_Does_Not_Become_Available_On_Exception()
+        {
+            var taskCompletionSource = new TaskCompletionSource<int>();
+            var taskLazy = new TaskLazy<int>(taskCompletionSource.Task);
+
+            taskCompletionSource.SetException(new Exception());
+
+            Assert.IsFalse(taskLazy.IsReady);
+            Assert.Catch<InvalidOperationException>(() =>
+            {
+                var temp = taskLazy.Value;
+            });
+            Assert.AreEqual(42, taskLazy.ValueOrDefault(42));
+            Assert.AreEqual(0, (int)taskLazy);
+        }
+    }
+}
---
+++ SingleThreadScheduler/SingleThreadScheduler/TaskLazy.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace journeyofcode.Threading
+{
+    /// <summary>
+    ///     Provides access to a value that will become available at some point in the future.
+    /// </summary>
+    /// <remarks>
+    ///     This type provides implicit conversions to <typeparamref name="T"/> and <see cref="Task&lt;T&gt;"/>. The implicit conversion to <typeparamref name="T"/> calls <see cref="ValueOrDefault"/>.
+    /// </remarks>
+    /// <typeparam name="T">
+    ///     The type of the value.
+    /// </typeparam>
+    public struct TaskLazy<T>
+    {
+        /// <summary>
+        ///     Returns <see cref="ValueOrDefault"/>.
+        /// </summary>
+        public static implicit operator T(TaskLazy<T> taskLazy)
+        {
+            return taskLazy.ValueOrDefault();
+        }
+        /// <summary>
+        ///     Returns <see cref="Task"/>.
+        /// </summary>
+        public static implicit operator Task<T>(TaskLazy<T> taskLazy)
+        {
+            return taskLazy.Task;
+        }
+
+        /// <summary>
+        ///     Returns a new <see cref="TaskLazy&lt;T&gt;" /> based on <paramref name="task"/>.
+        /// </summary>
+        public static implicit operator TaskLazy<T>(Task<T> task)
+        {
+            return new TaskLazy<T>(task);
+        }
+
+        /// <summary>
+        ///     Returns a new <see cref="TaskLazy&lt;T&gt;" /> which is initialized with the value <paramref name="value"/>.
+        /// </summary>
+        public static implicit operator TaskLazy<T>(T value)
+        {
+            return System.Threading.Tasks.Task.FromResult(value);
+        }
+
+        /// <summary>
+        ///     Returns the <see cref="Task"/> which is used to retrieve the value of this <see cref="TaskLazy&lt;T&gt;"/>
+        /// </summary>
+        public Task<T> Task { get; private set; }
+
+        /// <summary>
+        ///     Returns a value indicating whether the <see cref="Value"/> is available.
+        /// </summary>
+        public bool IsReady
+        {
+            get { return this.Task.IsCompleted && !this.Task.IsFaulted; }
+        }
+
+        /// <summary>
+        ///     Returns the Value or throws an exception if it is not yet available.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">
+        ///     Thrown when <see cref="IsReady"/> was <c>false</c> when calling this property.
+        /// </exception>
+        public T Value
+        {
+            get
+            {
+                if (!this.IsReady)
+                    throw new InvalidOperationException("Cannot access Value before it is available.");
+
+                return this.Task.Result;
+            }
+        }
+
+        /// <summary>
+        ///     Creates a new <see cref="TaskLazy&lt;T&gt;" /> based on a constant value.
+        /// </summary>
+        /// <param name="value">
+        ///     The value to use for <see cref="Value"/>.
+        /// </param>
+        /// <remarks>
+        ///     <see cref="IsReady"/> will be <c>true</c>.
+        /// </remarks>
+        public TaskLazy(T value)
+            : this(System.Threading.Tasks.Task.FromResult(value))
+        {
+            
+        }
+        /// <summary>
+        ///     Creates a new <see cref="TaskLazy&lt;T&gt;" /> based on a <see cref="Func&lt;T&gt;"/> which will be invoked in a new <see cref="Task"/>.
+        /// </summary>
+        /// <param name="func">
+        ///     The function to execute.
+        /// </param>
+        /// <remarks>
+        ///     The function will run on <see cref="TaskScheduler.Default"/>.
+        /// </remarks>
+        public TaskLazy(Func<T> func)
+            : this(System.Threading.Tasks.Task.Run(func))
+        {
+
+        }
+        /// <summary>
+        ///     Creates a new <see cref="TaskLazy&lt;T&gt;" /> based on an existing <see cref="Task&lt;T&gt;"/>.
+        /// </summary>
+        /// <param name="task">
+        ///     The task to use.
+        /// </param>
+        /// <remarks>
+        ///     You can also directly assign a <see cref="Task&lt;T&gt;"/> to a <see cref="TaskLazy&lt;T&gt;"/> variable.
+        /// </remarks>
+        public TaskLazy(Task<T> task)
+            : this()
+        {
+            this.Task = task;
+        }
+
+        /// <summary>
+        ///     Calls an <see cref="Action"/> in the current synchronisation context after the <see cref="Value"/> has become available.
+        /// </summary>
+        /// <param name="action">
+        ///     The <see cref="Action"/> to perform.
+        /// </param>
+        /// <remarks>
+        ///     This method uses <see cref="TaskScheduler.Current"/>.
+        /// </remarks>
+        public Task WhenValueAvailable(Action action)
+        {
+            return this.WhenValueAvailable(t => action());
+        }
+        /// <summary>
+        ///     Calls an <see cref="Action"/> in the current synchronisation context after the <see cref="Value"/> has become available.
+        /// </summary>
+        /// <param name="action">
+        ///     The <see cref="Action"/> to perform (<see cref="Value"/> is passed as a parameter).
+        /// </param>
+        /// <remarks>
+        ///     This method uses <see cref="TaskScheduler.Current"/>.
+        /// </remarks>
+        public Task WhenValueAvailable(Action<T> action)
+        {
+            return this.WhenValueAvailableAsync(action, TaskScheduler.Current);
+        }
+
+        /// <summary>
+        ///     Calls an <see cref="Action"/> using a specified <see cref="TaskScheduler"/> after the <see cref="Value"/> has become available.
+        /// </summary>
+        /// <param name="action">
+        ///     The <see cref="Action"/> to perform (<see cref="Value"/> is passed as a parameter).
+        /// </param>
+        /// <param name="taskScheduler">
+        ///     The <see cref="TaskScheduler"/> to perform <paramref name="action"/> on or <c>null</c> to use <see cref="TaskScheduler.Default"/>.
+        /// </param>
+        public Task WhenValueAvailableAsync(Action<T> action, TaskScheduler taskScheduler = null)
+        {
+            return this.Task.ContinueWith(task => action(task.Result), taskScheduler ?? TaskScheduler.Default);
+        }
+
+        /// <summary>
+        ///     Tries to retrieve the <see cref="Value"/> if it is available.
+        /// </summary>
+        /// <param name="result">
+        ///     Is set to <see cref="Value"/>.
+        /// </param>
+        /// <returns>
+        ///     <c>true</c> when <see cref="IsReady"/>, otherwise <c>false</c>.
+        /// </returns>
+        public bool TryGetValue(out T result)
+        {
+            if (!this.IsReady)
+            {
+                result = default(T);
+                return false;
+            }
+
+            result = this.Value;
+            return true;
+        }
+        /// <summary>
+        ///     Returns <see cref="Value"/> if it is available or <paramref name="def"/> otherwise.
+        /// </summary>
+        /// <param name="def">
+        ///     The value ro return when <see cref="Value"/> is not available (defaults to <c>default(T)</c>.
+        /// </param>
+        /// <returns>
+        ///     <see cref="Value"/> or <paramref name="def"/>.
+        /// </returns>
+        public T ValueOrDefault(T def = default(T))
+        {
+            T result;
+            if (!this.TryGetValue(out result))
+                return def;
+
+            return result;
+        }
+
+        /// <summary>
+        ///     Returns the value of a <see cref="Func&lt;T, T2&gt;"/> which receives <see cref="Value"/> as a paremeter or <paramref name="def"/> if the value is not available yet.
+        /// </summary>
+        /// <typeparam name="T2">
+        ///     The return value.
+        /// </typeparam>
+        /// <param name="func">
+        ///     The function to call when the value is available.
+        /// </param>
+        /// <param name="def">
+        ///     The value to return when the value is not available yet.
+        /// </param>
+        /// <returns>
+        ///     The return value of <paramref name="func"/> or <paramref name="def"/>.
+        /// </returns>
+        /// <example>
+        /// <![CDATA[
+        /// TaskLazy<DateTime> taskLazy = //...
+        /// string value = taskLazy.ActionOrDefault(date => date.ToString(), "Loading...");
+        /// ]]>
+        /// </example>
+        public T2 ActionOrDefault<T2>(Func<T, T2> func, T2 def = default(T2))
+        {
+            if (!this.Task.IsCompleted)
+                return def;
+
+            return func(this.Task.Result);
+        }
+    }
+}
